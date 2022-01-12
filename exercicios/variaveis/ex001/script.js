a2fce53fcac2678d177155ca971ada63ca885939
Mike <32079316+Eradamus@users.noreply.github.com>
2022-01-12 11:05:32 -0300

    Initial commit

---
+++ exercicios/variaveis/ex001/script.js
@@ -0,0 +1,24 @@
+var nome = 'Michell';
+var idade = 21;
+var comidaFavorita;
+comidaFavorita = "Arroz e Feijão";
+
+var a,b,c,d,e;
+alert('Oi');
+// console.log(nome, idade);
+// console.log(aindanãodefini);
+// function aindanãodefini{}
+
+var nome = 'Michell';//string
+var idade = 21;//number
+var possuiFaculdade = true;//boolean
+var time; //undefined
+var comida = null;//null
+var simbolo = Symbol() //Symbol
+var novoObjeto = {};
+
+console.log(typeof nome);
+
+var gols = 1000;
+var frase1 = "Romário fez " +  gols + " gols";
+console.log(frase1);
---
+++ exercicios/variaveis/ex002/script1.js
@@ -0,0 +1,10 @@
+var temstring = 'Não acredito';
+var numStr = '25';
+var idade = 21;
+var nome = 'Michell';
+var sobrenome = 'Silva';
+var nomeCompleto = `${nome}  ${sobrenome}` // Usar crase;
+console.log(nomeCompleto);
+
+var fraserandom = "It's Time" ;
+console.log(typeof fraserandom);
