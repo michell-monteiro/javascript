754168c6958dbfa6c994b6cd2c4a5deb2f18b6be
Mike <michellchristofferson@gmail.com>
2022-01-16 15:44:54 -0300

    Adicionados mais divs

--- projetos-teste/switch-card/js/flipcard.js
+++ projetos-teste/switch-card/js/flipcard.js
@@ -13,3 +13,31 @@ function flipcard2() {
     var rand = Math.floor(Math.random() * 12);
     document.getElementById("c3").style.backgroundColor = cpenbox[rand];
 }
+function flipcard3() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c4").style.backgroundColor = cpenbox[rand];
+}
+function flipcard4() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c5").style.backgroundColor = cpenbox[rand];
+}
+function flipcard5() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c6").style.backgroundColor = cpenbox[rand];
+}
+function flipcard6() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c7").style.backgroundColor = cpenbox[rand];
+}
+function flipcard7() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c8").style.backgroundColor = cpenbox[rand];
+}
+function flipcard8() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c9").style.backgroundColor = cpenbox[rand];
+}
+function flipcard9() {
+    var rand = Math.floor(Math.random() * 12);
+    document.getElementById("c10").style.backgroundColor = cpenbox[rand];
+}
