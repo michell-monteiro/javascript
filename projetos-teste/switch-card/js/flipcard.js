f6483158f7128911e6ab3d2376fa92b1bf061314
Mike <michellchristofferson@gmail.com>
2022-01-16 20:21:39 -0300

    código refeito

--- projetos-teste/switch-card/js/flipcard.js
+++ projetos-teste/switch-card/js/flipcard.js
@@ -1,43 +1,7 @@
 // abreviação de 'color pencil box' ou 'caixa de lápis de cor'
 var cpenbox = ["black", "brown", "yellowgreen", "green", "aquamarine", "blue", "purple", "palevioletred", "pink", "red", "orange", "yellow"];
 
-function flipcard() {
+function flipcard(id) {
     var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c1").style.backgroundColor = cpenbox[rand];
-}
-function flipcard1() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c2").style.backgroundColor = cpenbox[rand];
-}
-function flipcard2() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c3").style.backgroundColor = cpenbox[rand];
-}
-function flipcard3() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c4").style.backgroundColor = cpenbox[rand];
-}
-function flipcard4() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c5").style.backgroundColor = cpenbox[rand];
-}
-function flipcard5() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c6").style.backgroundColor = cpenbox[rand];
-}
-function flipcard6() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c7").style.backgroundColor = cpenbox[rand];
-}
-function flipcard7() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c8").style.backgroundColor = cpenbox[rand];
-}
-function flipcard8() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c9").style.backgroundColor = cpenbox[rand];
-}
-function flipcard9() {
-    var rand = Math.floor(Math.random() * 12);
-    document.getElementById("c10").style.backgroundColor = cpenbox[rand];
+    document.getElementById(id).style.backgroundColor = cpenbox[rand];
 }
