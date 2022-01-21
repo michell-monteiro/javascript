ce1e7ea1211fb4aaf2cdf0db6aea486302c71abd
Mike <michellchristofferson@gmail.com>
2022-01-21 18:56:30 -0300

    Update GameObject.js

--- projetos-teste/pizza-legends/GameObject.js
+++ projetos-teste/pizza-legends/GameObject.js
@@ -3,6 +3,9 @@ class GameObject {
         // determina a posição de spawn do objeto
         this.x = config.x || 0;
         this.y = config.y || 0;
-        this.sprite = null;
+        this.sprite = new Sprite({
+            gameObject: this,
+            src: config.src || "images/characters/people/hero.png",
+        });
     }
 }
