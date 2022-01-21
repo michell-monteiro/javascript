e851c23194cae820e323740d6a7ac34536d97bcf
Mike <michellchristofferson@gmail.com>
2022-01-21 17:17:11 -0300

    Criado um método

---
+++ projetos-teste/pizza-legends/init.js
@@ -0,0 +1,10 @@
+(function() {
+
+    // Inicia um objeto chamado overworld da classe Overworld
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+    // Chama o método init() do objeto overworld da classe Overworld
+    overworld.init();
+
+})();
