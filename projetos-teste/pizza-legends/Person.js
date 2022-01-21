28426728b802cf42743410ffeb31287c190832f5
Mike <michellchristofferson@gmail.com>
2022-01-21 20:45:57 -0300

    Ajustes nos métodos de direção

--- projetos-teste/pizza-legends/Person.js
+++ projetos-teste/pizza-legends/Person.js
@@ -1,6 +1,29 @@
 class Person extends GameObject {
     constructor(config) {
         super(config);
-        this.movingProgressRemaining = 16;
+        //Movimenta o personagem de acordo com o valor restante (32)
+        this.movingProgressRemaining = 32;
+
+        this.direction = "down";
+
+        this.directionUpdate = {
+            "up": ["y", -1],
+            "down": ["y", 1],
+            "left": ["x", -1],
+            "right": ["x", 1]
+        }
+    }
+
+    update(state) {
+        this.updatePosition();
+    }
+
+    // Verifica em qual posição o personagem está
+    updatePosition() {
+        if(this.movingProgressRemaining > 0) {
+            const [property, change] = this.directionUpdate[this.direction];
+            this[property] += change;
+            this.movingProgressRemaining -= 1;
+        }
     }
 }
