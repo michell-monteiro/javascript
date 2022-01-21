c7c0d4fbec8cef0261b90036ee084d83ec964e9d
Mike <michellchristofferson@gmail.com>
2022-01-21 19:45:11 -0300

    Criada a classe OverWorld

---
+++ projetos-teste/pizza-legends/OverworldMap.js
@@ -0,0 +1,55 @@
+class OverworldMap {
+    constructor(config) {
+        // Configura as camadas (layers) do mapa. Se um objeto deve estar acima ou abaixo de algo
+        this.gameObjects = config.gameObjects;
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx) {
+        ctx.drawImage(this.lowerImage, 0, 0)
+    }
+
+    drawUpperImage(ctx) {
+        ctx.drawImage(this.upperImage, 0, 0)
+    }
+}
+
+//A configuração de todos os mapas
+window.OverworldMap = {
+    DemoRoom: {
+        lowerSrc: "images/maps/DemoLower.png",
+        upperSrc: "images/maps/DemoUpper.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 5,
+                y: 6,
+            }),
+            npc1: new GameObject ({
+                x: 7,
+                y: 9,
+                src: "images/characters/people/npc1.png"
+            })
+        }
+    },
+    Kitchen: {
+        lowerSrc: "images/maps/KitchenLower.png",
+        upperSrc: "images/maps/KitchenUpper.png",
+        gameObjects: {
+            npc3: new GameObject({
+                x: 3,
+                y: 1,
+                src: "images/characters/people/npc3.png"
+            }),
+            npc5: new GameObject ({
+                x: 9,
+                y: 2,
+                src: "images/characters/people/npc5.png"
+            })
+        }
+    },
+}
