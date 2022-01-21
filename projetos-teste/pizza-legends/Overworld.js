5263f8673a70c8de86a2c56a7464cec04ac32f1c
Mike <michellchristofferson@gmail.com>
2022-01-21 17:52:28 -0300

    personagem adicionado
    são necessários 9 parâmetros para desenhar o personagem na tela

--- projetos-teste/pizza-legends/Overworld.js
+++ projetos-teste/pizza-legends/Overworld.js
@@ -13,6 +13,23 @@ class Overworld {
         };
         image.src = "images/maps/DemoLower.png";
 
+        // Desenha o personagem principal na tela
+        const x = 0;
+        const y = 0;
         const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, // left cut
+                0, // Top cut
+                32, // width of cut
+                32, // height of cut
+                x, // Position x to draw
+                y, // position y to draw
+                32, // Tamanho natural do sprite
+                32
+                );
+        }
+        hero.src = "images/characters/people/hero.png"
     }
 }
