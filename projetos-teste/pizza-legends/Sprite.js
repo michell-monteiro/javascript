31a5f7d7b4e41dc69b0da4b910a33183a275ebb8
Mike <michellchristofferson@gmail.com>
2022-01-21 18:51:44 -0300

    adicionado draw()

--- projetos-teste/pizza-legends/Sprite.js
+++ projetos-teste/pizza-legends/Sprite.js
@@ -1,6 +1,13 @@
 class Sprite {
     constructor(config) {
 
+        //Selecionar a imagem
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;
+        }
+
         // Configura a animação e o estado inicial
         this.animations = config.animations || {
             idleDown: [
@@ -10,5 +17,20 @@ class Sprite {
         // Determina qual frame será utilizado. Por padrão será 'idleDown'
         this.currentAnimation = config.currentAnimation || "idleDown";
         this.currentAnimationFrame = 0;
+
+        // Referência do game Object
+        this.gameObject = config.gameObject;
+    }
+
+    draw(ctx) {
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.y * 16 - 18;
+
+        ctx.drawImage(this.Image,
+            0,0,
+            32,32,
+            x,y,
+            32,32
+        )
     }
 }
