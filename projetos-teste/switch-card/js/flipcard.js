f8f8dd0187fb35728437974f05cc98a2073df12f
Mike <michellchristofferson@gmail.com>
2022-03-06 16:26:37 -0300

    nome para hexa...
    Criando uma nova funcionalidade usando o hexadecimal das cores.

--- projetos-teste/switch-card/js/flipcard.js
+++ projetos-teste/switch-card/js/flipcard.js
@@ -1,5 +1,8 @@
 // abreviação de 'color pencil box' ou 'caixa de lápis de cor'
-var cpenbox = ["black", "brown", "yellowgreen", "green", "aquamarine", "blue", "purple", "palevioletred", "pink", "red", "orange", "yellow"];
+//Em ordem sequencial, as cores em hexadecimal seguem a mesma ordem do array comentado abaixo.
+// var cpenbox = ["black", "brown", "yellowgreen", "green", "aquamarine", "blue", "purple", "palevioletred", "pink", "red", "orange", "yellow"];
+
+var cpenbox = ["#000000", "#a52a2a", "#9acd32", "#008000", "#7fffd4", "#0000ff", "#800080", "#db7093", "#ffc0cb", "#ff0000", "#ffa500", "#ffff00"];
 
 function flipcard(id) {
     var rand = Math.floor(Math.random() * 12);
