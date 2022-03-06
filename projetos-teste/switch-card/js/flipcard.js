5de54cfc69c4da65e66ba72fec8d9205ef7713df
Mike <michellchristofferson@gmail.com>
2022-03-06 17:04:20 -0300

    var flop
    armazena valor gerados por um Math.random dentro de um array.

--- projetos-teste/switch-card/js/flipcard.js
+++ projetos-teste/switch-card/js/flipcard.js
@@ -2,7 +2,15 @@
 //Em ordem sequencial, as cores em hexadecimal seguem a mesma ordem do array comentado abaixo.
 // var cpenbox = ["black", "brown", "yellowgreen", "green", "aquamarine", "blue", "purple", "palevioletred", "pink", "red", "orange", "yellow"];
 
-var cpenbox = ["#000000", "#a52a2a", "#9acd32", "#008000", "#7fffd4", "#0000ff", "#800080", "#db7093", "#ffc0cb", "#ff0000", "#ffa500", "#ffff00"];
+// var cpenbox = ["#000000", "#a52a2a", "#9acd32", "#008000", "#7fffd4", "#0000ff", "#800080", "#db7093", "#ffc0cb", "#ff0000", "#ffa500", "#ffff00"];
+
+var hexd = []
+for (a = 0; a < 6; a++){
+    var flop = Math.floor(Math.random() * 16);
+    hexd[a] = flop; 
+}
+
+
 
 function flipcard(id) {
     var rand = Math.floor(Math.random() * 12);
