2fbb7c3da3460d8f23b6f50f6092f939b4c3388b
Mike <michellchristofferson@gmail.com>
2022-03-06 17:10:34 -0300

    Update flipcard.js

--- projetos-teste/switch-card/js/flipcard.js
+++ projetos-teste/switch-card/js/flipcard.js
@@ -5,9 +5,38 @@
 // var cpenbox = ["#000000", "#a52a2a", "#9acd32", "#008000", "#7fffd4", "#0000ff", "#800080", "#db7093", "#ffc0cb", "#ff0000", "#ffa500", "#ffff00"];
 
 var hexd = []
+// Alimenta o array hexd
 for (a = 0; a < 6; a++){
     var flop = Math.floor(Math.random() * 16);
     hexd[a] = flop; 
+}
+
+// Inspeciona cada index do array
+for (b = 0; b < 6; b++) {
+    if (hexd[b] > 9) {
+        switch(hexd[b]) {
+            case 10:
+                hexd[b] = 'a';
+                break;
+            case 11:
+                hexd[b] = 'b';
+                break;
+            case 12:
+                hexd[b] = 'c';
+                break;
+            case 13:
+                hexd[b] = 'd';
+                break;
+            case 14:
+                hexd[b] = 'e';
+                break;
+            case 15:
+                hexd[b] = 'f';
+                break;
+            default: 
+                hexd[b] = 0;
+        }
+    }
 }
 
 
